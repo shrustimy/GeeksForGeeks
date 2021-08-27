e95f3f29b19f669104d965e41c281cf753294de7
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-08-28 00:25:54 +0530

    Update Find transition Point.cpp

--- Easy/Find transition Point.cpp
+++ Easy/Find transition Point.cpp
@@ -1,6 +1,9 @@
-/* Find Transition Point 
+/* Find Transition Point (link to the question --> https://practice.geeksforgeeks.org/problems/find-transition-point-1587115620/1)
 
-Difficulty level- Easy
+Difficulty level- Easy               TOPIC TAGS - Arrays , Searching
+
+Expected Time Complexity: O(LogN)
+Expected Auxiliary Space: O(1)
 
 Given a sorted array containing only 0s and 1s, find the transition point. 
 
@@ -26,9 +29,6 @@ Your Task:
 You don't need to read input or print anything. The task is to complete the function transitionPoint() that takes
 array and N as input parameters and returns the 0 based index of the position where "0" ends and "1" begins. If 
 array does not have any 1s, return -1. If array does not have any 0s, return 0.
-
-Expected Time Complexity: O(LogN)
-Expected Auxiliary Space: O(1)
 
 Constraints:
 1 ≤ N ≤ 500000
