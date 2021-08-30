54117bb827934a6020d9a662a0adb3ee519af919
Sanjana RP <sanjurps100@gmail.com>
2021-08-30 16:48:04 +0530

    Added new solution

--- Easy/Bitonic Point.cpp
+++ Easy/Bitonic Point.cpp
@@ -52,3 +52,21 @@ public:
 	    }
 	}
 };
+
+
+//Solution 2: Linear traversal
+
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	    // code here
+	    int max = arr[0];
+	    for(int i=0;i<n;i++){
+	        if(arr[i]>max){
+	            max=arr[i];
+	        }
+	    }
+	    return max;
+	}
+};
