68e35c7ab12b889fada9bb1d115c20a9efeff9c4
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-10-29 23:58:35 +0530

    Create soln.cpp

---
+++ Easy/Subarray with Sum/soln.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        // Your code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<=n-i+1;j++)
+            {
+                
+            }
+        }
+    }
+};
